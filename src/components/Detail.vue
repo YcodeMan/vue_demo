ef188ed3da2a6af722c5f14b54874d9c14f85da7
YcodeMan <1130578809@qq.com>
2019-05-26 14:33:53 +0800

    add src

---
+++ src/components/Detail.vue
@@ -0,0 +1,3 @@
+<template>
+    <a href="#">aaaaa</a>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
+import toastr from 'toastr'
+import MuseUI  from 'muse-ui'
+import Vodal from 'vodal'
+import router from './router'
+import 'toastr/build/toastr.min.css'
+import 'muse-ui/dist/muse-ui.css'
+import axios from 'axios'
+
+
+
+Vue.use(MuseUI)
+Vue.prototype.$axios = axios
+Vue.prototype.toastr = toastr
+Vue.component(Vodal.name,Vodal)
 
 Vue.config.productionTip = false
 
+
+toastr.options = {
+  "closeButton": true,
+  "debug": false,
+  "newestOnTop": false,
+  "progressBar": false,
+  "positionClass": "toast-top-right",
+  "preventDuplicates": false,
+  "onclick": null,
+  "showDuration": "300",
+  "hideDuration": "1000",
+  "timeOut": "3000",
+  "extendedTimeOut": "1000",
+  "showEasing": "swing",
+  "hideEasing": "linear",
+  "showMethod": "fadeIn",
+  "hideMethod": "fadeOut"
+}
+
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
